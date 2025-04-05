5f20aec5fb58b8944acca7c9b84497949e57c03c
Peter <peter.delafosse@gmail.com>
2025-04-05 16:00:17 +1100

    Parsing in chunks

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"bytes"
 	"errors"
 	"io"
 	"strings"
@@ -8,6 +9,7 @@ import (
 
 type Request struct {
 	RequestLine RequestLine
+	ParseStatus parseStatus
 }
 
 type RequestLine struct {
@@ -21,35 +23,74 @@ const (
 	CRLF         = "\r\n"
 )
 
+type parseStatus int
+
+const (
+	parseStatusInitialised parseStatus = iota
+	parseStatusDone
+)
+
 func RequestFromReader(reader io.Reader) (*Request, error) {
 
-	rawRequest, err := io.ReadAll(reader)
-	if err != nil || len(rawRequest) == 0 {
-		return nil, errors.New("unable to read request: " + err.Error())
-	}
-
-	requestParts := strings.Split(string(rawRequest), CRLF)
-
-	requestLine, err := parseRequestLine(requestParts[0])
-	if err != nil {
-		return nil, errors.New("unable to parse request line: " + err.Error())
-	}
-
 	req := Request{
-		RequestLine: requestLine,
+		ParseStatus: parseStatusInitialised,
+	}
+
+	for req.ParseStatus != parseStatusDone {
+
+		buf := bytes.NewBuffer(make([]byte, 0, 8))
+		n, err := buf.ReadFrom(reader)
+
+		if err != nil {
+			return nil, errors.New("error reading request: " + err.Error())
+		}
+
+		p, err := req.parse(buf.Bytes())
+
+		if err != nil {
+			return nil, errors.New("error parsing request: " + err.Error())
+		}
+
+		if n == p {
+			req.ParseStatus = parseStatusDone
+		}
+
 	}
 
 	return &req, nil
 
 }
 
-func parseRequestLine(line string) (RequestLine, error) {
+func (r *Request) parse(data []byte) (int64, error) {
+
+	bytesRead, requestLine, err := parseRequestLine(data)
+	if err != nil {
+		return 0, errors.New("error while parsing request line: " + err.Error())
+	}
+
+	if bytesRead == 0 {
+		return 0, nil
+	}
+
+	r.RequestLine = requestLine
+	return bytesRead, nil
+
+}
+
+func parseRequestLine(data []byte) (int64, RequestLine, error) {
+
+	nextCLRF := bytes.Index(data, []byte(CRLF))
+	if nextCLRF <= 0 {
+		return 0, RequestLine{}, nil
+	}
+
+	line := string(data[:nextCLRF])
 
 	parts := strings.Split(line, " ")
 	requestLine := RequestLine{}
 
 	if len(parts) != 3 {
-		return requestLine, errors.New("invalid request line: " + line)
+		return 0, requestLine, errors.New("invalid request line: " + line)
 	}
 
 	requestLine.Method = parts[0]
@@ -58,14 +99,16 @@ func parseRequestLine(line string) (RequestLine, error) {
 
 	for _, m := range requestLine.Method {
 		if m < 'A' || m > 'Z' {
-			return requestLine, errors.New("invalid request method: " + requestLine.Method)
+			return 0, requestLine, errors.New("invalid request method: " + requestLine.Method)
 		}
 	}
 
 	if requestLine.HttpVersion != HTTP_VERSION {
-		return requestLine, errors.New("invalid http version: " + requestLine.HttpVersion + ", expecting: " + HTTP_VERSION)
+		return 0, requestLine, errors.New("invalid http version: " + requestLine.HttpVersion + ", expecting: " + HTTP_VERSION)
 	}
 
-	return requestLine, nil
+	requestLine.HttpVersion = "1.1"
+
+	return int64(len(line)), requestLine, nil
 
 }
